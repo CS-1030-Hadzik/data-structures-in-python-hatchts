0f7b3449fedf46f4c2a8f26f24551c9694a6b7e2
hatchts <shatch86@gmail.com>
2022-12-01 18:13:58 -0700

    Create array.py

---
+++ array.py
@@ -0,0 +1,29 @@
+print("Stacy Hatch")
+
+#`problem 2. In the array.py create an array named 'cars' with the following elements in this order  ---- Ford,Chrysler,Dodge,Ram,Jeep,Chevy,GMC` (use single quotes for each element)EX: 'Ford' not "Ford" spelling matters
+cars =['Ford','Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+print(cars)
+
+print(len(cars))
+
+cars.append('Buick')
+
+print(cars)
+
+print(cars[3])
+
+
+cars.insert(2,'Toyota')
+
+
+print(cars)
+
+
+cars.pop(4)
+
+print(cars)
+
+
+
+
