db499d208f0c11d7ad490143abe4bfbc1c84557a
hatchts <shatch86@gmail.com>
2022-12-01 18:33:54 -0700

    Update array.py

--- array.py
+++ array.py
@@ -47,7 +47,7 @@ print(cars)
 my_array_length = len(cars)
 
 # problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)
-array_string = 'The length of my array is'
+array_string = 'The length of my array is '
 
 #  problem 18. print array_string concatenated with my_array_length to the console.`
 print(array_string + str(my_array_length))
@@ -56,7 +56,7 @@ print(array_string + str(my_array_length))
 
 # cars --> plural
 
-# car --> sing -- one element
+# car --> singular -- one element
 
 
 print('===================================================================================')
