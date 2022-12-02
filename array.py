446e3a45856774833bd30d31da243a865fa9d406
hatchts <shatch86@gmail.com>
2022-12-01 18:20:41 -0700

    Update array.py

--- array.py
+++ array.py
@@ -1,29 +1,41 @@
+# problem 1. print your first and last name`
 print("Stacy Hatch")
 
 #`problem 2. In the array.py create an array named 'cars' with the following elements in this order  ---- Ford,Chrysler,Dodge,Ram,Jeep,Chevy,GMC` (use single quotes for each element)EX: 'Ford' not "Ford" spelling matters
 cars =['Ford','Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
 
+#problem 3. print the array to the console`
 print(cars)
 
+# `problem 4. print the length of the array to the console `
 print(len(cars))
 
+# problem 5. Append Buick to the Array`
 cars.append('Buick')
 
+# problem 6. print the array to the console`
 print(cars)
 
+# problem 7. Print the 4th element in the array (Not index 4, element 4)`
 print(cars[3])
 
-
+# problem 8. Insert 'Toyota' into element 3 in the array`
 cars.insert(2,'Toyota')
 
-
+# problem 9. print the array to the console`
 print(cars)
 
-
+# problem 10. Remove element 5 of the array (hint look at options for pop())`
 cars.pop(4)
 
+# problem 11. print the array to the console`
 print(cars)
 
+# problem 12. Sort the array in ascending order`
+cars.sort
+
+# problem 13. print the array to the console`
+print(cars)
 
 
 
